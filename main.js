29ac908881a2a42446b8b0f27d5b4ad85f5d4c10
Arkadi <mrarkadi@gmail.com>
2017-02-10 19:38:10 +0200

    added 10.2.2017 version of reservations

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 var reservations = {
   'Bob': { claimed: false },
   'Ted': { claimed: true }
+};
+
+var claimReservation = function (name) {
+  var isNameFound = false;
+  for (var key in reservations) {
+    if (name.toLowerCase() === key.toLowerCase()) {
+      isNameFound = true;
+      if (reservations[key].claimed) {
+        alert("Hello " + name + ". Your reservation is ALREAY claimed");
+      }
+      else {
+        alert("Hello " + name + ". Your reservation is NOT YET claimed");
+        reservations[key].claimed = true;
+      }
+      break;
+    }
+  }
+  if (isNameFound === false) {
+    alert("Hello " + name + ". There is nothing under your name, You will be now added");
+    reservations[name] = { claimed: false };
+  }
+};
+
+var addName = function(name) {
+  var newReservation = document.createElement("li");
+  var newName = document.createTextNode(name);
+  newReservation.appendChild(newName);
+  var reservationList = document.getElementById("list");
+  reservationList.appendChild(newReservation);
+};
+
+// before clicking on anything - I want to display my current database
+for (var key in reservations) {
+    addName(key);
 }
 
-var name = prompt('Please enter the name for your reservation');
+document.getElementById("button").onclick = function() {
+  name = document.getElementById("input").value;
+  claimReservation(name);
+  addName(name);
+};
 
-var claimReservation = function () {
-  // write your code here!
-}
-
-claimReservation();
+console.log(reservations);
